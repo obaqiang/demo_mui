115084287b4a5fc118d217abc1919f7093859e13
qiangqiangbuzhidao <qiangqiangbuzhidao@DESKTOP-I8LFEUJ>
2017-04-20 15:32:28 +0800

    @2017/4/20

--- js/common/bdapp.js
+++ js/common/bdapp.js
@@ -394,14 +394,30 @@ function getNowFormatDate(newDate) {
 	var seperator2 = ":";
 	var month = date.getMonth() + 1;
 	var strDate = date.getDate();
+	
 	if(month >= 1 && month <= 9) {
 		month = "0" + month;
 	}
+	console.log(month);
 	if(strDate >= 0 && strDate <= 9) {
 		strDate = "0" + strDate;
 	}
+	console.log(strDate);
+	var hours = date.getHours();
+	if(hours<10){
+		hours = '0'+hours;
+	}
+	var minutes = date.getMinutes();
+	if(minutes<10){
+		minutes = '0'+minutes;
+	}
+	var seconds = date.getSeconds();
+	if(seconds<10){
+		seconds = '0'+seconds;
+	}
+	console.log(date.getMinutes());
 	var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
-		" " + date.getHours() + seperator2 + date.getMinutes() +
-		seperator2 + date.getSeconds();
+		" " + hours + seperator2 + minutes +
+		seperator2 + seconds;
 	return currentdate;
 }
--- js/config/web_config.js
+++ js/config/web_config.js
@@ -1,7 +1,7 @@
 //var test_url='http://192.168.1.163';
 
-var yehongbing_url='http://192.168.1.192';
-//var yehongbing_url='https://testapi.bdvip.net';
-//var test_url = 'https://testapi.bdvip.net';
+//var yehongbing_url='http://192.168.1.192';
+var yehongbing_url='http://testapi.bdvip.net';
+var test_url = 'http://testapi.bdvip.net';
 //var test_url = 'http://192.168.1.192';
-var test_url = 'http://192.168.1.170';
+//var test_url = 'http://192.168.1.170';
